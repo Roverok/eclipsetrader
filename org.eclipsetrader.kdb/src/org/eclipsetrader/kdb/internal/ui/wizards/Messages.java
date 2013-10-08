bb5b0618ded5eb4a18e980813a01ba45bafccd6c
nfukue <nfukue@3a13df04-2589-4aa1-8dcf-759cc2d0c8df>
2013-10-08 16:21:08 +0000

    k-db plugin.
    git-svn-id: http://svn.sourceforge.jp/svnroot/eclipsetrader-j/trunk/eclipsetrader-j@68 3a13df04-2589-4aa1-8dcf-759cc2d0c8df

---
+++ org.eclipsetrader.kdb/src/org/eclipsetrader/kdb/internal/ui/wizards/Messages.java
@@ -0,0 +1,28 @@
+package org.eclipsetrader.kdb.internal.ui.wizards;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipsetrader.kdb.internal.ui.wizards.messages"; //$NON-NLS-1$
+	public static String DataImportWizard_ImportDataFromKDB;
+	public static String ImportDataPage_Aggregation;
+	public static String ImportDataPage_AllSecurities;
+	public static String ImportDataPage_Days;
+	public static String ImportDataPage_Full;
+	public static String ImportDataPage_FullIncremental;
+	public static String ImportDataPage_Import;
+	public static String ImportDataPage_Incremental;
+	public static String ImportDataPage_Minutes;
+	public static String ImportDataPage_Period;
+	public static String ImportDataPage_SecuritiesSelectedBelow;
+	public static String ImportDataPage_SelectImportSecurity;
+	public static String ImportDataPage_To;
+	public static String ImportDataPage_TYpe;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
