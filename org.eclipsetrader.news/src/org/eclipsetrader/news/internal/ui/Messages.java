3d9d641297446f2efdf5beb0d39249b6ea520f48
nfukue <nfukue@3a13df04-2589-4aa1-8dcf-759cc2d0c8df>
2013-10-12 06:01:45 +0000

    Japanese translation
    git-svn-id: http://svn.sourceforge.jp/svnroot/eclipsetrader-j/trunk/eclipsetrader-j@76 3a13df04-2589-4aa1-8dcf-759cc2d0c8df

--- org.eclipsetrader.news/src/org/eclipsetrader/news/internal/ui/Messages.java
+++ org.eclipsetrader.news/src/org/eclipsetrader/news/internal/ui/Messages.java
@@ -5,6 +5,12 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipsetrader.news.internal.ui.messages"; //$NON-NLS-1$
 	public static String HeadLineViewer_Date;
+	public static String HeadLineViewer_MarkAllAsRead;
+	public static String HeadLineViewer_MarkAsRead;
+	public static String HeadLineViewer_Next;
+	public static String HeadLineViewer_Open;
+	public static String HeadLineViewer_Previous;
+	public static String HeadLineViewer_Refresh;
 	public static String HeadLineViewer_Security;
 	public static String HeadLineViewer_Source;
 	public static String HeadLineViewer_Title;
